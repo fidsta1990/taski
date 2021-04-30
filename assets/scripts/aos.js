dbc4c27d54ddb64a81d7962d2817ecc22f750310
Fidel Collin <fidelcollin@icloud.com>
2021-04-30 17:39:22 +0100

    added animation to alls ection

---
+++ assets/scripts/aos.js
@@ -0,0 +1,9 @@
+AOS.init({
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 500, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+});
